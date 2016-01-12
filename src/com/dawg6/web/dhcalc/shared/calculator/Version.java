7fa21e67e79060ed6a8ab3b1408bf0440b88914f
dawg6 <scott@dawg6.com>
2016-01-12 14:41:46 -0500

    Updated changelog and build

--- src/com/dawg6/web/dhcalc/shared/calculator/Version.java
+++ src/com/dawg6/web/dhcalc/shared/calculator/Version.java
@@ -28,8 +28,8 @@ public class Version  implements Serializable {
 
 	public static final String PREFIX = "Version:";
 	
-	public static String VERSION = "2.3-160104.2";
-	public static String VERSION_DATE = "01/04/16";
+	public static String VERSION = "2.3-160112.1";
+	public static String VERSION_DATE = "01/12/16";
 	
 	public String version;
 	
