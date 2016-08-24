71c5f8942fa7fa93934d42d0f30a01e8e384ed8e
dawg6 <scott@dawg6.com>
2016-08-24 15:37:03 -0400

    Added hatred to chart, added chart zoom, added chart context menu.

--- src/com/dawg6/web/dhcalc/client/GraphPanel.java
+++ src/com/dawg6/web/dhcalc/client/GraphPanel.java
@@ -23,9 +23,19 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.Vector;
 
+import org.moxieapps.gwt.highcharts.client.AxisTitle;
+import org.moxieapps.gwt.highcharts.client.BaseChart;
+import org.moxieapps.gwt.highcharts.client.Button.Align;
 import org.moxieapps.gwt.highcharts.client.Chart;
+import org.moxieapps.gwt.highcharts.client.ContextButton;
+import org.moxieapps.gwt.highcharts.client.Exporting;
 import org.moxieapps.gwt.highcharts.client.Point;
 import org.moxieapps.gwt.highcharts.client.Series;
+import org.moxieapps.gwt.highcharts.client.Style;
+import org.moxieapps.gwt.highcharts.client.labels.AxisLabelsData;
+import org.moxieapps.gwt.highcharts.client.labels.AxisLabelsFormatter;
+import org.moxieapps.gwt.highcharts.client.labels.XAxisLabels;
+import org.moxieapps.gwt.highcharts.client.labels.YAxisLabels;
 
 import com.dawg6.gwt.client.widgets.SimpleCaptionPanel;
 import com.dawg6.web.dhcalc.shared.calculator.Damage;
@@ -46,6 +56,7 @@ public class GraphPanel extends Composite {
 	private final CheckBox target;
 	private final CheckBox dps;
 	private final CheckBox targetHp;
+	private final CheckBox hatred;
 
 	public GraphPanel() {
 
@@ -54,8 +65,6 @@ public class GraphPanel extends Composite {
 
 		table = new FlexTable();
 		panel.setWidget(table);
-
-		resetChart();
 
 		SimpleCaptionPanel options = new SimpleCaptionPanel("Options");
 		table.setWidget(1, 0, options);
@@ -110,6 +119,20 @@ public class GraphPanel extends Composite {
 
 			}
 		});
+
+		hatred = new CheckBox("Show Hatred");
+		table2.setWidget(row, column++, hatred);
+		hatred.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				redraw();
+
+			}
+		});
+
+		resetChart();
+		table.setWidget(0, 0, chart);
 	}
 
 	private void resetChart() {
@@ -118,16 +141,93 @@ public class GraphPanel extends Composite {
 		chart.setChartTitleText("Damage Log");
 		chart.setWidth("800px");
 		chart.setHeight("600px");
-		chart.getXAxis().setAxisTitleText("Time");
+		AxisTitle x = new AxisTitle();
+		x.setText("Time");
+		x.setStyle(new Style().setColor("black").setFontWeight("bold"));
+		chart.getXAxis().setAxisTitle(x);
 		chart.getXAxis().setMin(0);
-		chart.getYAxis(0).setAxisTitleText("Damage");
-		chart.getYAxis(0).setMin(0);
-		chart.getYAxis(1).setAxisTitleText("HP Percent");
-		chart.getYAxis(1).setMin(0);
-		chart.getYAxis(1).setMax(100.0);
-		chart.getYAxis(1).setOpposite(true);
-		table.setWidget(0, 0, chart);
+		chart.getXAxis().setLabels(
+				new XAxisLabels().setStyle(new Style().setColor("black")
+						.setFontWeight("bold")));
 
+		AxisTitle y0 = new AxisTitle();
+		y0.setText("Damage");
+		y0.setStyle(new Style().setColor("black").setFontWeight("bold"));
+		chart.getYAxis(0).setAxisTitle(y0);
+		chart.getYAxis(0).setLabels(
+				new YAxisLabels().setFormatter(new AxisLabelsFormatter() {
+
+					@Override
+					public String format(AxisLabelsData axisLabelsData) {
+						double d = axisLabelsData.getValueAsDouble();
+
+						if (d < 1000)
+							return String.valueOf((long) d);
+						else if (d < 1000000)
+							return String.valueOf((long) d / 1000) + "K";
+						else if (d < 100000000)
+							return String.valueOf((long) d / 1000000) + "M";
+						else
+							return String.valueOf((long) d / 1000000000) + "B";
+					}
+				})
+						.setStyle(
+								new Style().setColor("black").setFontWeight(
+										"bold")));
+
+		chart.getYAxis(0).setMin(0);
+
+		if (this.targetHp.getValue()) {
+			chart.getYAxis(1).setMin(0);
+			chart.getYAxis(1).setMax(100.0);
+			chart.getYAxis(1).setOpposite(true);
+			AxisTitle y1 = new AxisTitle();
+			y1.setText("HP Percent");
+			y1.setStyle(new Style().setColor("green").setFontWeight("bold"));
+			chart.getYAxis(1).setAxisTitle(y1);
+			chart.getYAxis(1).setLabels(
+					new YAxisLabels().setFormatter(new AxisLabelsFormatter() {
+
+						@Override
+						public String format(AxisLabelsData axisLabelsData) {
+
+							if (axisLabelsData.getValueAsDouble() > 100)
+								return "";
+							else
+								return axisLabelsData.getValueAsString() + "%";
+						}
+					})
+							.setStyle(
+									new Style().setColor("green")
+											.setFontWeight("bold")));
+		}
+
+		if (this.hatred.getValue()) {
+			chart.getYAxis(2).setMin(0);
+			AxisTitle y2 = new AxisTitle();
+			y2.setText("Hatred");
+			y2.setStyle(new Style().setColor("red").setFontWeight("bold"));
+			chart.getYAxis(2).setAxisTitle(y2);
+			chart.getYAxis(2).setOpposite(true);
+			chart.getYAxis(2).setLabels(
+					new YAxisLabels().setFormatter(new AxisLabelsFormatter() {
+
+						@Override
+						public String format(AxisLabelsData axisLabelsData) {
+							return String.valueOf(axisLabelsData
+									.getValueAsLong());
+						}
+					}).setStyle(
+							new Style().setColor("red").setFontWeight("bold")));
+
+		}
+		
+		
+		ContextButton button =  new ContextButton();
+		button.setAlign(Align.RIGHT);
+		
+		chart.setExporting(new Exporting().setContextButton(button));
+		chart.setZoomType(BaseChart.ZoomType.X_AND_Y);
 	}
 
 	public void setLog(DamageResult log) {
@@ -147,6 +247,7 @@ public class GraphPanel extends Composite {
 		boolean showTarget = target.getValue();
 		boolean showDps = dps.getValue();
 		boolean showTargetHp = targetHp.getValue();
+		boolean showHatred = hatred.getValue();
 
 		List<Series> series = new Vector<Series>();
 		Map<String, Integer> map = new TreeMap<String, Integer>();
@@ -157,10 +258,13 @@ public class GraphPanel extends Composite {
 		List<Point> dpsPoints = new Vector<Point>();
 		double dpsPrev = -1;
 
+		double max[] = { 0.0, 100.0, 125.0 };
+
 		if (!showSkills && !showTarget) {
 			Series s = chart.createSeries();
 			s.setYAxis(0);
 			s.setName("Damage Done");
+			s.setOption("color", "black");
 			chart.addSeries(s);
 			series.add(s);
 			points.add(new Vector<Point>());
@@ -170,28 +274,66 @@ public class GraphPanel extends Composite {
 		if (showDps) {
 			dpsSeries = chart.createSeries();
 			dpsSeries.setName("DPS");
+			dpsSeries.setOption("color", "blue");
 			chart.addSeries(dpsSeries);
 			dpsSeries.setYAxis(0);
 		}
 
 		for (Damage d : log.damages) {
+			{
+				if (showHatred) {
+					String source = "Hatred";
+					Integer index = map.get(source);
+
+					if (index == null) {
+						index = series.size();
+						map.put(source, index);
+						Series s = chart.createSeries();
+						s.setOption("color", "red");
+						s.setYAxis(2);
+						s.setName(source);
+						chart.addSeries(s);
+						series.add(s);
+						points.add(new Vector<Point>());
+						prev.add(new Double(-1));
+					}
+
+					List<Point> pt = points.get(index);
+					Double pp = prev.get(index);
+
+					if (d.time > pp) {
+						pt.add(new Point(d.time, d.currentHatred));
+						prev.set(index, d.time);
+					} else {
+						int n = pt.size() - 1;
+						Point p = pt.get(n);
+						pt.set(n, new Point(p.getX(), d.currentHatred));
+					}
+
+					max[2] = Math.max(max[2], d.currentHatred);
+				}
+
+			}
+
 			if (d.damage > 0) {
 
 				if (showDps) {
 					totalDamage += d.damage;
 					double duration = d.time;
-					
+
 					if (duration == 0)
 						duration = 1.0;
-					
+
+					double dps = totalDamage / duration;
+
+					max[0] = Math.max(max[0], dps);
+
 					if (d.time > dpsPrev) {
-						dpsPoints.add(new Point(d.time, totalDamage
-								/ duration));
+						dpsPoints.add(new Point(d.time, dps));
 					} else {
 						int n = dpsPoints.size();
 						Point p = dpsPoints.get(n);
-						dpsPoints.set(n, new Point(p.getX(), totalDamage
-								/ duration));
+						dpsPoints.set(n, new Point(p.getX(), dps));
 					}
 				}
 
@@ -203,6 +345,7 @@ public class GraphPanel extends Composite {
 						index = series.size();
 						map.put(source, index);
 						Series s = chart.createSeries();
+						s.setOption("color", "green");
 						s.setYAxis(1);
 						s.setName(source);
 						chart.addSeries(s);
@@ -219,51 +362,56 @@ public class GraphPanel extends Composite {
 					} else {
 						int n = pt.size() - 1;
 						Point p = pt.get(n);
-						pt.set(n, new Point(p.getX(), d.targetHpPercent * 100.0));
+						pt.set(n,
+								new Point(p.getX(), d.targetHpPercent * 100.0));
 					}
 				}
 
-				Integer index = 0;
+				{
+					Integer index = 0;
 
-				if (showSkills || showTarget) {
-					StringBuffer buf = new StringBuffer();
+					if (showSkills || showTarget) {
+						StringBuffer buf = new StringBuffer();
 
-					if (showTarget)
-						buf.append(d.target.toString());
-
-					if (showSkills) {
 						if (showTarget)
-							buf.append("-");
-						buf.append(d.source.getName());
+							buf.append(d.target.toString());
+
+						if (showSkills) {
+							if (showTarget)
+								buf.append("-");
+							buf.append(d.source.getName());
+						}
+
+						String source = buf.toString();
+						index = map.get(source);
+
+						if (index == null) {
+							index = series.size();
+							map.put(source, index);
+							Series s = chart.createSeries();
+							s.setYAxis(0);
+							s.setName(source);
+							chart.addSeries(s);
+							series.add(s);
+							points.add(new Vector<Point>());
+							prev.add(new Double(-1));
+						}
 					}
 
-					String source = buf.toString();
-					index = map.get(source);
+					List<Point> pt = points.get(index);
+					Double pp = prev.get(index);
 
-					if (index == null) {
-						index = series.size();
-						map.put(source, index);
-						Series s = chart.createSeries();
-						s.setYAxis(0);
-						s.setName(source);
-						chart.addSeries(s);
-						series.add(s);
-						points.add(new Vector<Point>());
-						prev.add(new Double(-1));
+					if (d.time > pp) {
+						pt.add(new Point(d.time, d.damage));
+						prev.set(index, d.time);
+						max[0] = Math.max(max[0], d.damage);
+					} else {
+						int n = pt.size() - 1;
+						Point p = pt.get(n);
+						double dd = p.getY().doubleValue() + d.damage;
+						pt.set(n, new Point(p.getX(), dd));
+						max[0] = Math.max(max[0], dd);
 					}
-				}
-
-				List<Point> pt = points.get(index);
-				Double pp = prev.get(index);
-
-				if (d.time > pp) {
-					pt.add(new Point(d.time, d.damage));
-					prev.set(index, d.time);
-				} else {
-					int n = pt.size() - 1;
-					Point p = pt.get(n);
-					pt.set(n, new Point(p.getX(), p.getY().doubleValue()
-							+ d.damage));
 				}
 			}
 		}
@@ -274,6 +422,16 @@ public class GraphPanel extends Composite {
 		for (int i = 0; i < series.size(); i++) {
 			series.get(i).setPoints(points.get(i).toArray(new Point[0]));
 		}
+
+		for (int i = 0; i < max.length; i++) {
+
+			if ((i == 0) || ((i == 1) && showTargetHp)
+					|| ((i == 2) && showHatred)) {
+				chart.getYAxis(i).setExtremes(0, max[i], false, false);
+			}
+		}
+
+		table.setWidget(0, 0, chart);
 	}
 
 }
